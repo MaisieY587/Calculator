e0b76e9bc13df7ab3791048637f4dc9b120579a9
Maisie Yan <maisiey78@gmail.com>
2024-07-08 15:57:55 -0400

    created basic operate function that takes in first number, operate, and second number

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let first;
+let operator;
+let second;
+
+function operate(first, operator, second) {
+    if (operator === '+') {
+        add(first, second);
+    }
+    if (operator === '-') {
+        subtract(first, second);
+    }
+    if (operator === '*') {
+        multiply(first, second);
+    }
+    if (operator === '/') {
+        divide(first, second);
+    }
+}
